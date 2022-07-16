3003405f812fc44b516444ac5967f336c8656bea
Saurabh <raisaurabh265@gmail.com>
2022-07-17 04:06:07 +0530

    exapmle graphql querry runing

---
+++ server.js
@@ -0,0 +1,24 @@
+import { ApolloServer, gql } from "apollo-server";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+
+const typeDefs = gql`
+  type Query {
+    greet: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greet: () => "Hello world!",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
