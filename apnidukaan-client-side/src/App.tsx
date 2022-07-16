147606b4c6eb653c8e0ff5e012ccc1264648596f
Saurabh <raisaurabh265@gmail.com>
2022-07-17 02:28:19 +0530

    project started

---
+++ apnidukaan-client-side/src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        <h1
+          style={{
+            color: "red",
+          }}
+        >
+          Apni Dukaan
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.ts
@@ -0,0 +1,25 @@
+import { ApolloServer , gql } from "apollo-server";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+
+const typeDefs = gql`
+
+type Query {
+    greet: String
+}
+
+`
+
+const resolvers = {
+    Query: {
+        greet: () => "Hello World"
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+})
